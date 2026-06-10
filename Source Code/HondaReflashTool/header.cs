8cef39351e54b61fdb31103c7b811663c9fbf78b
Jakka351 <jakka351@outlook.com>
2026-06-11 02:39:07 +1000

    Create header.cs

---
+++ Source Code/HondaReflashTool/header.cs
@@ -0,0 +1,115 @@
+// ////////////////////////////////////////////////////////////////////////////////////////////////////
+//                            Tester Present Specialist Automotive Solutions                         //
+//                   2026 Jack Leighton. github.com/jakka351/Open-Tester-Engineering                 //
+// ////////////////////////////////////////////////////////////////////////////////////////////////////                                                                     
+//                                           .  ....       .            ..                            
+//                                          .:::. .:.... ...           .=+.                           
+//                                           .::::::::::::::.         .=++-.                          
+//                                          .:::::::::::::..          .++++:                          
+//                                 .:-=.   .::::::::::::::.:          :++++-                          
+//                               .=+++++-:..:::::::::::::.  .         :+++++-=.                       
+//                              :+++++++++:::::::::::::::::...        :++++++++.                      
+//                           ...-+++++++++::::::::::::::::::::.  .    +++++++++:                      
+//                         .-=.+++++++++++-:::::::::::::::::::-++:  .=+++++++++=.                     
+//                         -++++++++++++++-:::::::::::::::::::-+++++++++++++++++-                     
+//                        .-++++++++++++++-:::::::::::::::::::=+++++++++++++++++-                     
+//                       .=+++++++++++++++-:::::::::::::::::::=++++++++++++++++++..                   
+//                     .:+++++++++++++++++=:::::::::::::::::::=++++++++++++++++++++=.                 
+//                ..-+++++++++++++++++++++=:::::::::::::::::::+++++++++++++++++++++++=.               
+//            .:=+++++++++++++++++++++++++=:::::::::::::::::::++++++++++++++++++++++++-               
+//         ..:++++++++++++++++++++++++++++=:::::::::::::::::::+++++++++++++++++++++++++.              
+//        :.+++++++++++++++++++++++++++++++:::::::::::::::::::+++++++++++++++++++++++++++-.           
+//        -++++++++++++++++++++++++++++++++.::::JAKKA351::::::+++++++++++++++++++++++++++=            
+//        :++++++++++++++++++++++++++++++++.::::::::::::::::::++++++++++++++++++++++++++++=.          
+//        -++++++++++++++++++++++++++++++++.:-------------::::======++++++++++++++++++++++++.         
+//        .=+++++++++++++++++++++++++++++++-+++++++++++++++++++++++=+++++++++++++++++++++++++:        
+//        .+-++++++++++++++++++++++++++++++-+++++++++++++++++++++++=+++++++++++++++++++++++++-        
+//        .:+++++++++++++++++++++++++++++++-+++++++++++++++++++++++=+++++++++++++++++++++++++:        
+//          .++++++++++++++++++++++++++++++-+++++++++++++++++++++++----======++++++++==++=+==:        
+//           .+++++++++++++++++++++++++++++=+++++++++++++++++++++++-===============-=========-.       
+//             =++++++++++++++++++++++++++++=++++++++++++++++++++++-=========================.        
+//             .++++++++++++++++++++++++++++=++==++++++++++++++++++-========================..        
+//              :+++++++++++++++++++++++=:.       ..:-+++++++++++++-=======================-.         
+//               .+++++++++++++++++=:.               .-+++++==+++++=======================-.          
+//               .++++++++++++++++:                    .+++:.++++++-====================-:.           
+//               .=++++++++:......                     .==  =:+++++=+:=================:              
+//              .-++++++=.                                 ..:+++++=+++:==========--=-.               
+//                 ..:..                                  .... .=+=+++++=----==-===--:.               
+//                                                              -+=+++++++++++++-==-.                 
+//                                                              .=-+++++++++++++=:-:.                 
+//                                                                ..:=+++++++++:::.                   
+//                                                                    ..  .:-.                        
+//         TESTER PRESENT SPECIALIST AUTOMOTIVE SOLUTIONS              .:.     .                       
+//                                                                     ....   ..                      
+//                                                                      :=====-                       
+//                                                                      .=====:                       
+//                                                                      .-===-.                       
+//                                                                        ::.                         
+//
+/*
+ * Open-Tester-Engineering
+ * Third-Party Open Source Component
+ *
+ * ------------------------------------------------------------
+ * FILE PURPOSE
+ * ------------------------------------------------------------
+ * This file is part of the Open-Tester-Engineering repository,
+ * which contains open-source components derived from third-party
+ * software used for automotive communication and diagnostic tooling.
+ *
+ * This file may be:
+ *  - original open-source code
+ *  - a modified version of open-source code
+ *  - a port of open-source code into C#
+ *
+ * ------------------------------------------------------------
+ * COPYRIGHT & OWNERSHIP
+ * ------------------------------------------------------------
+ * Original authors retain all rights to their respective work.
+ * Any modifications contained herein are attributed below.
+ *
+ * No proprietary code from Tester Engineering Suite is included
+ * in this file.
+ *
+ * ------------------------------------------------------------
+ * ORIGINAL SOURCE (IF APPLICABLE)
+ * ------------------------------------------------------------
+ * Project Name:      HondaReflashTool
+ * Original Author:   bouletmarc
+ * Source URL:        https://github.com/bouletmarc/HondaReflashTool
+ * License:           None
+ *
+ * ------------------------------------------------------------
+ * MODIFICATIONS
+ * ------------------------------------------------------------
+ * The following modifications have been made from the original source:
+ *
+ * - Ported to C# from C#
+ * - Refactored into modular architecture for CAN/UDS abstraction
+ * - Removed platform-specific dependencies
+ * - Adapted interfaces for integration with Open-Tester-Engineering
+ *
+ * ------------------------------------------------------------
+ * LICENSE
+ * ------------------------------------------------------------
+ * This file is distributed under the terms of its original license.
+ *
+ * - GPL-licensed components remain subject to the GNU General Public License
+ * - MIT-licensed components remain under the MIT License
+ *
+ * See the LICENSES/ directory for full license texts.
+ *
+ * ------------------------------------------------------------
+ * IMPORTANT SEPARATION NOTICE
+ * ------------------------------------------------------------
+ * This file is strictly part of the open-source compliance layer.
+ *
+ * It is NOT part of any proprietary system, including (but not limited to)
+ * Tester Engineering Suite, and is not linked, compiled, or distributed
+ * with any closed-source components.
+ *
+ * Any interaction with proprietary systems must occur via external
+ * interfaces and is not represented in this file.
+ *
+ * ------------------------------------------------------------
+ */
